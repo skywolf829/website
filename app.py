409b69c4852c593a307ac06acee505475f625a3b
Skylar Wurster <swwurster@gmail.com>
2021-02-06 12:04:10 -0500

    Updates templates to simplfy

--- app.py
+++ app.py
@@ -5,56 +5,64 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('HTML_start.html') + \
-        render_template('HTML_head.html') + \
-        render_template('HTML_bodystart.html') + \
-        render_template('HTML_sidebar.html') + \
-        render_template('index_body.html') + \
-        render_template('HTML_bodyend.html') + \
-        render_template("HTML_end.html")
+    return '<!DOCTYPE html><html lang="en">' + \
+            "<head>" + \
+            render_template('head_items.html') + \
+            "</head><body>" + \
+            render_template('sidebar.html') + \
+            render_template('/index_body.html') + \
+           "</body></html>" 
 
 @app.route('/projects')
 def project():
-    return render_template('HTML_start.html') + \
-        render_template('HTML_head.html') + \
-        render_template('HTML_bodystart.html') + \
-        render_template('HTML_sidebar.html') + \
-        render_template('/pages/projects_body.html') + \
-        render_template('HTML_bodyend.html') + \
-        render_template("HTML_end.html")
+    return '<!DOCTYPE html><html lang="en">' + \
+            "<head>" + \
+            render_template('head_items.html') + \
+            "</head><body>" + \
+            render_template('sidebar.html') + \
+            render_template('/pages/projects_body.html') + \
+           "</body></html>" 
 
 @app.route('/projects/<pagename>')
 def project_pages(pagename=None):
-    if(pagename == "CSE5542_finalproject.html"):
-        return render_template('/pages/projects/CSE5542_finalproject.html')
+    if(pagename == "CSE5542_finalproject.html"):        
+        return '<!DOCTYPE html><html lang="en">' + \
+            "<head>" + \
+            render_template('head_items.html') + \
+            render_template('/pages/projects/CSE5542_finalproject_head.html') + \ 
+            "</head><body id='lab5-body' onload='webGLStart();'>" + \
+            render_template('sidebar.html') + \
+            render_template('/pages/projects/CSE5542_finalproject_body.html') + \
+           "</body></html>" 
+
     else:
-        return render_template('HTML_start.html') + \
-            render_template('HTML_head.html') + \
-            render_template('HTML_bodystart.html') + \
-            render_template('HTML_sidebar.html') + \
+        return '<!DOCTYPE html><html lang="en">' + \
+            "<head>" + \
+            render_template('head_items.html') + \
+            "</head><body>" + \
+            render_template('sidebar.html') + \
             render_template('/pages/projects/'+pagename) + \
-            render_template('HTML_bodyend.html') + \
-            render_template("HTML_end.html")
+           "</body></html>" 
 
 @app.route('/teaching')
 def teaching():
-    return render_template('HTML_start.html') + \
-        render_template('HTML_head.html') + \
-        render_template('HTML_bodystart.html') + \
-        render_template('HTML_sidebar.html') + \
-        render_template('pages/teaching_body.html') + \
-        render_template('HTML_bodyend.html') + \
-        render_template("HTML_end.html")
+    return '<!DOCTYPE html><html lang="en">' + \
+            "<head>" + \
+            render_template('head_items.html') + \
+            "</head><body>" + \
+            render_template('sidebar.html') + \
+            render_template('/pages/teaching_body.html') + \
+           "</body></html>" 
 
 @app.route('/teaching/<pagename>')
 def teaching_pages(pagename=None):
-    return render_template('HTML_start.html') + \
-        render_template('HTML_head.html') + \
-        render_template('HTML_bodystart.html') + \
-        render_template('HTML_sidebar.html') + \
-        render_template('/pages/teaching/'+pagename) + \
-        render_template('HTML_bodyend.html') + \
-        render_template("HTML_end.html")
+    return '<!DOCTYPE html><html lang="en">' + \
+            "<head>" + \
+            render_template('head_items') + \
+            "</head><body>" + \
+            render_template('sidebar.html') + \
+            render_template('/pages/teaching/'+pagename) + \
+           "</body></html>" 
 
 if __name__ == '__main__':
     #app.run(host='127.0.0.1',debug=True,port="12345")
--- templates/HTML_bodystart.html
+++
@@ -1,1 +0,0 @@
-<body id="page-top">
